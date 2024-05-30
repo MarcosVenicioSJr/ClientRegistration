61039a263b88c89183b0e6022cf26649c4bf5047
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-05-30 08:42:44 -0300

    Initial commit

---
+++ Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using ClientRegistration.Models.Enums;
+
+namespace ClientRegistration.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Subscription Subscription { get; set; }
+    }
+}
---
+++ Models/Enums/Subscription.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientRegistration.Models.Enums
+{
+    public enum Subscription
+    {
+        Brass = 0,
+        Silver = 1,
+        Gold = 2,
+        Diamond = 3
+    }
+}
